716f07519e98d92b87a85587e00cec73440820df
brianstittsr <brianstittsr@gmail.com>
2025-10-31 10:02:20 -0400

    fix: Dynamically add Vercel URL to image domains

--- next.config.js
+++ next.config.js
@@ -1,29 +1,36 @@
 /** @type {import('next').NextConfig} */
+
+const remotePatterns = [
+  {
+    protocol: "https",
+    hostname: "cdn.sanity.io",
+    port: "",
+  },
+  {
+    protocol: "http",
+    hostname: "localhost",
+  },
+  {
+    protocol: "https",
+    hostname: "leadershipconnectionsnc.weebly.com",
+  },
+  {
+    protocol: "https",
+    hostname: "via.placeholder.com",
+  },
+];
+
+// Add the Vercel deployment URL to the list of allowed domains
+if (process.env.VERCEL_URL) {
+  remotePatterns.push({
+    protocol: "https",
+    hostname: process.env.VERCEL_URL,
+  });
+}
+
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-      },
-      {
-        protocol: "https",
-        hostname: "leadershipconnectionsnc.weebly.com",
-      },
-      {
-        protocol: "https",
-        hostname: "via.placeholder.com",
-      },
-      {
-        protocol: "https",
-        hostname: "**.vercel.app",
-      },
-    ],
+    remotePatterns,
   },
   // Ignore TypeScript errors during build
   typescript: {
